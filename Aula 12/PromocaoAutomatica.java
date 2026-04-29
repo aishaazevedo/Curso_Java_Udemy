d111336085c76fd07a7a20c7ddd7f2085eba7410
aishaazevedo <azevedoaisha@gmail.com>
2026-04-28 22:14:32 -0300

    Iniciando em java

--- Aula 12/PromocaoAutomatica.java
+++ Aula 12/PromocaoAutomatica.java
@@ -1,8 +1,6 @@
 public class PromocaoAutomatica {
     public static void main(String[] args) {
-        byte a = 10;
-        short b = 20;
-        char c = 'A';
+
         int d = 30;
         long e = 40L;
         float f = 1.5f;
