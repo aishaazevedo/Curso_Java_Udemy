059bf5d68ba5e649ccc2d4ccdbe699a3823eb172
aishaazevedo <azevedoaisha@gmail.com>
2026-04-29 11:02:36 -0300

    Exercícios da Aula 13

--- Aula 13/ParOuImpar.java
+++ Aula 13/ParOuImpar.java
@@ -2,7 +2,7 @@ public class ParOuImpar {
     public static void main(String[] args) {
 
         int num1 = 20;
-        int num2 = 30;
+        int num2 = 10;
 
         if (num1 % num2 != 0) {
             System.out.println("É ímpar!! " );
